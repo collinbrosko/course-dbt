a337507238b22c11a725c3852541c5ea6ebf1aa0
collinbrosko <collin.brosko@gocaribou.com>
2022-06-27 14:20:33 +0000

    Week 3

--- greenery/models/marts/product/intermediate/int_session_event_agg_static.sql
+++ greenery/models/marts/product/intermediate/int_session_event_agg_static.sql
@@ -7,8 +7,8 @@ config(
 
 SELECT 
 session_id
-, date(created_at) created_at
 , user_id
+, date(created_at) created_at
 , SUM(case when event_type = 'add_to_cart' then 1 else 0 end) as add_to_cart
 , SUM(case when event_type = 'checkout' then 1 else 0 end) as checkout
 , SUM(case when event_type = 'page_view' then 1 else 0 end) as page_view
